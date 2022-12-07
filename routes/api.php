6ab26c7ba866dc453776f00321152b83a1a3a616
SalmaKhoirunisaR <nisasalma335@gmail.com>
2022-12-07 18:37:29 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Middleware\UserCheck;
+use App\Http\Middleware\AdminCheck;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+// Route::post('register', 'UserController@register');
+// Route::post('login', 'UserController@login');
+
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    //public routes
+    Route::post('logout', [UserController::class, 'logout']);
+    Route::resource('ticket', AdminController::class, ['only' => ['index']]);
+    Route::resource('transaction', AdminController::class, ['only' => ['index']]);
+    
+    Route::middleware([UserCheck::class])->group(function () {
+        Route::resource('transaction', TransactionController::class, ['only' => ['store']]);
+    });
+
+    Route::middleware([AdminCheck::class])->group(function () {
+        Route::resource('ticket', AdminController::class, ['only' => [ 'store', 'update', 'destroy', 'show']]);     
+        Route::resource('transaction', AdminController::class, ['only' => [ 'update', 'destroy', 'show']]);     
+    });
+
+
+
+});
+
+
